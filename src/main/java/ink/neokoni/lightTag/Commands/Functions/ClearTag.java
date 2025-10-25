045388bd536f703cf6217a139e6480fce51f51f8
neokoni <dulin081682@126.com>
2025-10-26 03:23:23 +0800

    fix: notice only player can execute

--- src/main/java/ink/neokoni/lightTag/Commands/Functions/ClearTag.java
+++ src/main/java/ink/neokoni/lightTag/Commands/Functions/ClearTag.java
@@ -10,6 +10,7 @@ import org.bukkit.entity.Player;
 public class ClearTag {
     public ClearTag(CommandSender sender) {
         if (!(sender instanceof Player)) {
+            sender.sendMessage(TextUtils.getFormatedLang("system.player-only"));
             return;
         }
 
